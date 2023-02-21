51c850033460c4f7d61b7781300fb7e959b1d652
Sumedh Vartak <67580847+sumedh004@users.noreply.github.com>
2023-02-21 12:59:20 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -83,7 +83,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo usermod -aG docker cloud_user
 
 export JFROG_HOME=/opt/jfrog
